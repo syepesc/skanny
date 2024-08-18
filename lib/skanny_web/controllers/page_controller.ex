6ff9ce43e3c5ce1402c9bcc573af1af91dd33021
Santiago Yepes <santiagoyepescarrera@gmail.com>
2024-08-18 18:52:34 -0400

    Small cleanup

--- lib/skanny_web/controllers/page_controller.ex
+++ lib/skanny_web/controllers/page_controller.ex
@@ -1,9 +1,7 @@
 defmodule SkannyWeb.PageController do
   use SkannyWeb, :controller
 
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
+  # def home(conn, _params) do
+  #   render(conn, :home)
+  # end
 end
